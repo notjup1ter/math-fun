7f15a1166b86f81473a4ce82fa26f960ce109a9f
Jayden Brown <bjayden36@gmail.com>
2026-06-29 13:10:25 -0400

    optimized multiples function by using an iterator

--- src/multiples.rs
+++ src/multiples.rs
@@ -1,8 +1,4 @@
 //This is a function that will handle the sum of all multiples of 3 and 5 from 1 to any given n > 1
 pub fn sum_multiples_3or5(n:u32) -> u32 {
-    let mut sum: u32 = 0;
-    for i in 0..n {
-        if i % 3 == 0 || i % 5 == 0 {sum += i;}
-    }
-    sum
+    (0..n).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
 }
