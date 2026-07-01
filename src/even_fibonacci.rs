105d35ecdfa1373eda447777987d6474603c7ab8
Jayden Brown <bjayden36@gmail.com>
2026-07-01 11:40:25 -0400

    implemented faster version of the even fibonacci sum algorithm and implemented a unit test for it

--- src/even_fibonacci.rs
+++ src/even_fibonacci.rs
@@ -1,6 +1,8 @@
 /// Returns the sum of all even fibbonacci numbers that do not exceed 
 /// input n
 
+
+
 pub fn create_fib_sequence(n:u32) -> Vec<u32> {
     let mut fib_vec : Vec<u32> = vec![1, 2];
 
@@ -16,6 +18,18 @@ pub fn even_fib_sum(n: u32) -> u32 {
     create_fib_sequence(n).into_iter()
         .filter(|x| x % 2 == 0)
         .sum()
+}
+
+/// Constructs a vector of only even fibonacci numbers up to input n
+/// and returns its sum
+pub fn even_fib_sum_v2(n: u32) -> u32 {
+    let mut fib_vec: Vec<u32> = vec![0,2];
+
+    loop {
+        let temp_sum: u32 = 4 * fib_vec[fib_vec.len() - 1] + fib_vec[fib_vec.len() - 2];
+        if temp_sum > n {break;} else {fib_vec.push(temp_sum);}
+    }
+    fib_vec.into_iter().sum()
 }
 
 
@@ -40,4 +54,10 @@ mod tests {
     fn even_fib_sum_test() {
         assert_eq!(even_fib_sum(4000000), 4613732)
     } 
+
+    // Test for correctness on version 2 of the even sum algorithm
+    #[test]
+    fn even_fib_sum_v2_test() {
+        assert_eq!(even_fib_sum_v2(4000000), 4613732)
+    }
 }
