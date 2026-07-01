f56f18f1932bd8cc721ae181e661cbd98f961075
Jayden Brown <bjayden36@gmail.com>
2026-07-01 15:43:13 -0400

    updated inital max prime factor algorithm

--- src/max_prime_factor.rs
+++ src/max_prime_factor.rs
@@ -2,22 +2,22 @@
 /// Returns the highest prime number for a given input, n. 
 /// uses Sieve of Eratosthenes
 
-pub fn max_prime_factor(n: usize) -> u32 {
-    let mut is_prime: Vec<bool> = vec![true; n + 1];
-    let p: u32 = 2;
+pub fn max_prime_factor(n: i64) -> i64 {
+    let mut is_prime: Vec<bool> = vec![true; (n + 1) as usize];
+    let mut p: i64 = 2;
 
     while p * p <= (n) {
-        if is_prime[p] {
-            for i in (p*p..n+1).step_by(p) {
-                prime[i] = false;
+        if is_prime[p as usize] {
+            for i in (p*p..n+1).step_by(p as usize) {
+                is_prime[i as usize] = false;
             }
         }
         p += 1;
     }
 
-    let mut primes: Vec<f64> = Vec::<f64>::new();
+    let mut primes: Vec<i64> = Vec::<i64>::new();
     for num in 2..n+1 {
-        if is_prime[num] {primes.push(num);}
+        if is_prime[num as usize] {primes.push(num);}
     }
 
     primes[is_prime.len() - 1]
@@ -33,6 +33,6 @@ mod tests {
 
     #[test]
     fn max_prime_factor_test() {
-        assert_eq!(max_prime_factor(600851475143.0), 6857);
+        assert_eq!(max_prime_factor(600_851_475_143), 6857);
     }
 }
